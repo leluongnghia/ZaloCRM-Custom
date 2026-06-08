e97a5e429f9a91ef4fae2f2c701b56da390f2301
LeLuongNghia <leluongnghia@LeLuongNghias-MacBook-Pro.local>
2026-06-08 16:50:37 +0700

    fix: change import order in main.ts to load Tailwind before Vuetify

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,9 @@
+import './assets/main.css';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import { router } from './router/index';
 import { vuetify } from './plugins/vuetify';
-import './assets/main.css';
 
 const app = createApp(App);
 app.use(createPinia());
